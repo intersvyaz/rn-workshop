dff018ef3b47fcd3d457bcd5f7cccbef49599ff7
trifonov_is <trifonov_is@intersvyaz.net>
2019-04-05 17:55:30 +0500

    Step 2

--- Search.js
+++ Search.js
@@ -1,11 +1,14 @@
 import React, {Component} from "react";
-import {Text, View, FlatList} from "react-native";
+import {Text, View, FlatList, TextInput} from "react-native";
+
+import Api from "./Api";
 
 export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       series: [],
+      searchText: "",
     };
   }
 
@@ -18,6 +21,25 @@ export default class Search extends Component {
   componentDidMount() {
     console.log("Search");
   }
+
+  _handleSubmitEditing = () => {
+    Api.searchSeries(this.state.searchText)
+      .then(response => this.setState({series: response.data}))
+      .catch(error => console.log("searchSeries error", error));
+  };
+
+  _handleTextChange = text => this.setState({searchText: text});
+
+  _renderHeader = () => {
+    return (
+      <TextInput
+        placeholder={"Введите название сериала"}
+        onChangeText={this._handleTextChange}
+        onSubmitEditing={this._handleSubmitEditing}
+        style={{margin: 30, fontSize: 20, borderColor: "grey", padding: 10, borderWidth: 2, borderRadius: 5}}
+      />
+    );
+  };
 
   _renderFooter() {
     return (
@@ -39,6 +61,7 @@ export default class Search extends Component {
         data={this.state.series}
         extraData={this.state}
         renderItem={this._renderItem}
+        ListHeaderComponent={this._renderHeader}
         ListEmptyComponent={this._renderFooter}
         keyExtractor={item => item.id.toString()}
       />
